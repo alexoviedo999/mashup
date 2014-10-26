81f2ccd182e9fabf22a9b470e7cafb772ff7f514
alexoviedo <alexoviedo999@gmail.com>
2014-10-26 17:26:41 -0400

    loading pics in bs carousel.

--- js/main.js
+++ js/main.js
@@ -6,22 +6,51 @@ $.ajax({
         url: 'https://api.instagram.com/v1/media/popular?client_id=03a834da5a654e7a96d47ec72e1043f0',
         dataType: 'jsonp',
         success: function(result){
-          for (var i = 0; i < result.data.length; i++){
-            var url = result.data[i].images.thumbnail.url;
+          for (var i = 0; i < 4; i++){
+            var url = result.data[i].images.standard_resolution.url;
             var location = result.data[i].location;
 
             if (location){
+// '<div class="active item" data-url="'+ url +'" data-slide-number="0">'
+
 	            var appendElm = '<img src="' + url + '"/>'
-	            var $div = $('<div>');
-	            var cssUrl = 'url(' + url +')'
+	            // var $div = $('<div class="item" data-url="'+ url +'" data-slide-number="0">');
+	            var $div = $('<div class="item" data-slide-number="'+[i]+'"></div>');
+	            var cssUrl = 'url(' + url + ')'
 	            $div.addClass('photo');
 	            $div.css('background-image', cssUrl);
-	            $('#photos').prepend($div);
+	            $('.carousel-inner').append($div);
 	            // $($div).append(appendElm);
-	            $($div).prepend('<h1>" '+ location.latitude +'"</h1')[0];
+	            $($div).append('<h1>" '+ location.latitude +'"</h1')[0];
+
+
+	            $('#myCarousel').carousel({
+				  interval:false // remove interval for manual sliding
+				});
+
+				// when the carousel slides, load the ajax content
+				$('#myCarousel').on('slid', function (e) {
+				  
+					// get index of currently active item
+					var idx = $('#myCarousel .item.active').index();
+					// var url = $('.item.active').data('url');
+
+					// ajax load from data-url
+				  	$('.item').html("wait...");
+					$('.item').load(url,function(result){
+					    $('#myCarousel').carousel(idx);  
+					});
+				  
+				});
             }
           }
+          $('.item:nth-child(1)').addClass('active')
         }
       });
+
+
+
+
+
 
 });
